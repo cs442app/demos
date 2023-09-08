721c444fff924270877abdc198f4ede5ece11c75
Michael Lee <lee@soi2.org>
2023-09-08 10:57:20 -0500

    Example 1 of flutter intro

--- 01_flutter_intro/lib/main.dart
+++ 01_flutter_intro/lib/main.dart
@@ -22,8 +22,46 @@
  * - Stateful widgets
  */
 
-import 'package:flutter/material.dart';
+import 'dart:math';
+import 'package:flutter/Material.dart';
 
 void main() {
-  runApp(const Placeholder());
+  const exampleNum = 1; // can we hot-reload a change to this?
+                        // see https://docs.flutter.dev/tools/hot-reload#special-cases
+
+  switch(exampleNum) {
+    case 0:
+      // simplest possible app!
+      // (check out definitions of `runApp`, `Placeholder`, and superclasses)
+      runApp(const Placeholder());
+      break;
+
+    case 1:
+      // a custom widget
+      runApp(const App1()); // why the `const`?
+      break;
+  }
 }
+
+//*****************************************************************************
+
+class App1 extends StatelessWidget {
+  // why is this constructor `const`? (what happens if we remove it?)
+  // note the unused optional parameter `key` (we'll talk about this later)
+  const App1({super.key});
+
+  // `build` is called by the framework to build the widget tree
+  @override
+  Widget build(BuildContext context) {  // `context` is my location in the tree
+    // construct and return a widget tree
+    // (try to navigate the tree in the widget inspector)
+    return const Center(
+      child: Text(
+        'Hello world!', // try changing this and hot-reloading
+        textDirection: TextDirection.ltr, // what happens if we remove this?
+      )
+    );
+  }      
+}
+
+//*****************************************************************************
