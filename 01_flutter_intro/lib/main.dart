b53ef6dc9edf6ed6fa252cd2a369d6f7968a0272
Michael Lee <lee@soi2.org>
2023-09-08 11:03:04 -0500

    Adding the rest of the flutter intro examples

--- 01_flutter_intro/lib/main.dart
+++ 01_flutter_intro/lib/main.dart
@@ -26,7 +26,7 @@ import 'dart:math';
 import 'package:flutter/Material.dart';
 
 void main() {
-  const exampleNum = 1; // can we hot-reload a change to this?
+  const exampleNum = 2; // can we hot-reload a change to this?
                         // see https://docs.flutter.dev/tools/hot-reload#special-cases
 
   switch(exampleNum) {
@@ -39,6 +39,34 @@ void main() {
     case 1:
       // a custom widget
       runApp(const App1()); // why the `const`?
+      break;
+
+    case 2:
+      runApp(const App2());
+      break;
+
+    case 3:
+      // our first Material app (what is Material? what does it provide?)
+      // (check it out in the widget details tree)
+      runApp(const MaterialApp(
+        title: 'App3',
+        home: App3() // the "home" / root widget
+      ));
+      break;
+
+    case 4:
+      runApp(const MaterialApp(
+        title: 'App4',
+        home: App4(),
+      ));
+      break;
+
+    case 5:
+      runApp(const MaterialApp(
+        title: 'App5',
+        home: App5(),
+        debugShowCheckedModeBanner: false,
+      ));
       break;
   }
 }
@@ -65,3 +93,149 @@ class App1 extends StatelessWidget {
 }
 
 //*****************************************************************************
+
+class App2 extends StatelessWidget {
+  const App2({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Directionality(
+      // the following property is inherited by descendant widgets
+      textDirection: TextDirection.ltr,
+      child: Column(
+        // try to tweak this property in the layout explorer
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Text('Hello!'), // no need to specify `textDirection` here
+          const SizedBox(height: 50, width: 50), // spacer
+          GestureDetector(
+            child: const Text(
+              'World!',
+              style: TextStyle( // manually specified fancy style
+                color: Colors.red,
+                fontWeight: FontWeight.bold,
+                fontSize: 30
+              )
+            ),
+            // callback for when the widget is tapped
+            onTap: () => print('"World!" clicked'),
+          ),
+        ]
+      ),
+    );
+  }      
+}
+
+//*****************************************************************************
+
+class App3 extends StatelessWidget {
+  const App3({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold( // what happens if we remove the scaffold?
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Text(
+              "I'm a Material App!",
+              // where does this style come from?
+              style: Theme.of(context).textTheme.titleLarge
+            ),
+            // automatic gesture detection
+            // (try to find the GestureDetector in the widget details tree)
+            ElevatedButton(
+              child: const Text('Hey! Click me!'),
+              onPressed: () {
+                print('Button clicked');
+              }, 
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+//****************************************************************************
+
+class App4 extends StatelessWidget {
+  const App4({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('App4'),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.add),
+        onPressed: () {
+          print('Add button clicked');
+        }, 
+      ),
+      body: ListView(
+        // can you add the tiles 'Lions', 'Tigers', 'Bears', and 'Oh my!'?
+        children: [
+          ListTile(
+            title: Text('Hello World!'),
+            onTap: () => print('"Hello World!" clicked'),
+          ),
+        ]
+      ),
+    );
+  }
+}
+
+//*****************************************************************************
+
+// A stateful widget! Track down the superclass in the framework source code.
+class App5 extends StatefulWidget {
+  const App5({super.key});
+
+  // note: we don't override `build` here
+  @override
+  State<App5> createState() => _App5State();
+}
+
+
+// A state object! The state persists across tree rebuilds, and is bound to
+// its element in the tree. When the state changes, the widget is rebuilt.
+class _App5State extends State<App5> {
+  int _luckyNum = 0;
+
+  _App5State() {
+    _luckyNum = Random().nextInt(100);
+  }
+
+  void _generateLuckyNum() {
+    // what happens if we remove the call to `setState`?
+    setState(() {
+      print('Generating lucky number...');
+      _luckyNum = Random().nextInt(100);
+    });
+  }
+
+  // try setting a breakpoint here and clicking the button
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Lucky number generator'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Text('Your lucky number is: $_luckyNum'),
+            ElevatedButton(
+              onPressed: _generateLuckyNum, 
+              child: const Text('Get my lucky number'),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
