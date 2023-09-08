a8112df88a129e9cabee4ae80a6893b2fd57c670
Michael Lee <lee@soi2.org>
2023-09-08 10:45:41 -0500

    Adding Flutter intro project

---
+++ 01_flutter_intro/README.md
@@ -0,0 +1,3 @@
+# flutter_intro
+
+A new Flutter project.
---
+++ 01_flutter_intro/android/app/src/main/kotlin/com/example/flutter_intro/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_intro
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
