830b379febf5eedbb9ff4033a569e838d590d7af
Michael Lee <lee@soi2.org>
2024-06-05 14:31:02 -0500

    Removing future provider, cleaning up code, making UI consistent

--- 05_persistence/lib/views/eg5.dart
+++ 05_persistence/lib/views/eg5.dart
@@ -5,7 +5,6 @@
 
 import 'dart:math';
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 import 'package:word_generator/word_generator.dart';
 import 'package:intl/intl.dart';
 import '../models/orders.dart';
@@ -14,28 +13,14 @@ import '../utils/db_helper.dart';
 class App5 extends StatelessWidget {
   const App5({super.key});
 
-  Future<List<Customer>> _loadData() async {
-    final data = await DBHelper().query('customer');
-    return data.map((e) => Customer(
-      id: e['id'] as int,
-      name: e['name'] as String,
-      email: e['email'] as String,
-    )).toList();    
-  }
-
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: FutureProvider<List<Customer>?>(
-        create: (_) => _loadData(),
-        initialData: null,
-        child: const CustomerList()
-      ),
+    return const MaterialApp(
+      home: CustomerList(),
       debugShowCheckedModeBanner: false,
     );
   }
 }
-
 
 class CustomerList extends StatefulWidget {
   const CustomerList({super.key});
@@ -45,50 +30,75 @@ class CustomerList extends StatefulWidget {
 }
 
 class _CustomerListState extends State<CustomerList> {
+  late Future<List<Customer>> _customers;
+
+  @override
+  void initState() {
+    super.initState();
+    _customers = _loadData();
+  }
+
+  Future<List<Customer>> _loadData() async {
+    final data = await DBHelper().query('customer');
+    return data.map((c) => Customer(
+      id: c['id'] as int,
+      name: c['name'] as String,
+      email: c['email'] as String,
+    )).toList();    
+  }
+
   @override
   Widget build(BuildContext context) {
-    final people = Provider.of<List<Customer>?>(context);
-    
-    if (people == null) {
-      return const Scaffold(
-        body: Center(
-          child: CircularProgressIndicator(),
-        ),
-      );
-    }
+    return FutureBuilder(
+      future: _customers, 
+      initialData: const <Customer>[],
+      builder: (context, snapshot) {
+        final people = snapshot.data as List<Customer>; 
 
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Customers'),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () async {
-          final person = Customer(
-            name: WordGenerator().randomName(),
-            email: '${WordGenerator().randomNoun()}@example.com',
-          );
-          await person.dbSave();
-          setState(() {
-            people.add(person);
-          });
-        },
-        child: const Icon(Icons.add),
-      ),
-      body: ListView.builder(
-        itemCount: people.length,
-        itemBuilder: (context, index) {
-          final person = people[index];
-          return ListTile(
-            title: Text(person.name),
-            subtitle: Text(person.email),
-            onTap:() => Navigator.of(context).push(
-              MaterialPageRoute(
-                builder: (context) => OrderList(customer: person),
+        return Scaffold(
+          appBar: AppBar(
+            title: const Text('Customers'),
+            actions: [
+              IconButton(
+                icon: const Icon(Icons.add),
+                onPressed: () async {
+                  final person = Customer(
+                    name: WordGenerator().randomName(),
+                    email: '${WordGenerator().randomNoun()}@example.com',
+                  );
+                  await person.dbSave();
+                  setState(() {
+                    _customers = _loadData();
+                  });
+                },
               ),
-            )
-          );
-        },
-      ),
+              IconButton(
+                icon: const Icon(Icons.refresh),
+                onPressed: () {
+                  setState(() {
+                    _customers = _loadData();
+                  });
+                },
+              ),
+            ],
+          ),
+          body: ListView.builder(
+            itemCount: people.length,
+            itemBuilder: (context, index) {
+              final person = people[index];
+              return ListTile(
+                title: Text(person.name),
+                subtitle: Text(person.email),
+                onTap:() => Navigator.of(context).push(
+                  MaterialPageRoute(
+                    builder: (context) => OrderList(customer: person),
+                  ),
+                )
+              );
+            },
+          ),
+        );
+      }
     );
   }
 }
@@ -106,85 +116,78 @@ class OrderList extends StatefulWidget {
 class _OrderListState extends State<OrderList> {
   late Future<List<Order>> _orders;
 
-  Future<List<Order>> _loadData() async {
-    final data = await DBHelper().query(
-      'purchase_order', 
-      where: 'customer_id = ${widget.customer.id!}'
-    );
-    return data.map((e) => Order(
-      id: e['id'] as int,
-      description: e['description'] as String,
-      price: e['price'] as double,
-      customerId: e['customer_id'] as int,
-    )).toList();    
-  }
-
-
   @override
   void initState() {
     super.initState();
     _orders = _loadData();
   }
 
+  Future<List<Order>> _loadData() async {
+    final data = await DBHelper().query(
+      'purchase_order', 
+      where: 'customer_id = ${widget.customer.id!}'
+    );
+    return data.map((o) => Order(
+      id: o['id'] as int,
+      description: o['description'] as String,
+      price: o['price'] as double,
+      customerId: o['customer_id'] as int,
+    )).toList();    
+  }
+
   @override
   Widget build(BuildContext context) {
     return FutureBuilder(
       future: _orders, 
-      initialData: const [],
+      initialData: const <Order>[],
       builder: (context, snapshot) {
-        if (snapshot.connectionState == ConnectionState.waiting) {
-          return const Scaffold(
-            body: Center(
-              child: CircularProgressIndicator(),
-            ),
-          );
-        } else {
-          final orders = snapshot.data as List<Order>;
-          var formatter = NumberFormat.currency(locale: 'en_US', symbol: '\$');
+        final orders = snapshot.data as List<Order>;
+        var formatter = NumberFormat.currency(locale: 'en_US', symbol: '\$');
 
-          return Scaffold(
-            appBar: AppBar(
-              title: Text(widget.customer.name),
-            ),
-            floatingActionButton: FloatingActionButton(
-              onPressed: () async {
-                final order = Order(
-                  customerId: widget.customer.id!,
-                  price: Random().nextDouble() * 100,
-                  description: WordGenerator().randomSentence(),
-                );
-                await order.dbSave();
-                setState(() {
-                  orders.add(order);
-                });
-              },
-              child: const Icon(Icons.add),
-            ),
-            body: ListView.builder(
-              itemCount: orders.length,
-              itemBuilder: (context, index) {
-                final order = orders[index];
-                return Dismissible(
-                  key: Key(order.id.toString()), 
-                  onDismissed: (_) {
-                    order.dbDelete();
-                    setState(() {
-                      orders.removeAt(index);
-                    });
-                  },
-                  background: Container(
-                    color: Colors.red,
-                    child: const Icon(Icons.delete),
-                  ),
-                  child: ListTile(
-                    title: Text(order.description.toUpperCase()),
-                    subtitle: Text(formatter.format(order.price)),
-                  ),
-                );
-              },
-            ),
-          );
-        }
+        return Scaffold(
+          appBar: AppBar(
+            title: Text(widget.customer.name),
+            actions: [
+              IconButton(
+                icon: const Icon(Icons.add),
+                onPressed: () async {
+                  final order = Order(
+                    customerId: widget.customer.id!,
+                    price: Random().nextDouble() * 100,
+                    description: WordGenerator().randomSentence(),
+                  );
+                  await order.dbSave();
+                  setState(() {
+                    _orders = _loadData();
+                  });
+                },
+              )
+            ],
+          ),
+          body: ListView.builder(
+            itemCount: orders.length,
+            itemBuilder: (context, index) {
+              final order = orders[index];
+              return Dismissible(
+                key: Key(order.id.toString()), 
+                onDismissed: (_) {
+                  order.dbDelete();
+                  setState(() {
+                    orders.removeAt(index);
+                  });
+                },
+                background: Container(
+                  color: Colors.red,
+                  child: const Icon(Icons.delete),
+                ),
+                child: ListTile(
+                  title: Text(order.description.toUpperCase()),
+                  subtitle: Text(formatter.format(order.price)),
+                ),
+              );
+            },
+          ),
+        );
       }
     );
   }
