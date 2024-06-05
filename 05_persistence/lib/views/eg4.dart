cb15c8b80efa2db2a505cdf27b251a1ec7df82e6
Michael Lee <lee@soi2.org>
2024-06-05 14:30:34 -0500

    Removing FutureProvider and adding save/load
    Adding code to illustrate how to load data from assets bundle and write
    it to the documents directory

--- 05_persistence/lib/views/eg4.dart
+++ 05_persistence/lib/views/eg4.dart
@@ -1,105 +1,183 @@
 // Demonstrates:
-// - how to use the provider package's FutureProvider to deal with
-//   asynchronously loaded data
+// - defining a model class that loads/serializes itself from/to JSON
+// - the typical workflow of copying a file from the assets bundle to the 
+//   data directory and writing changes there
 
+import 'dart:io';
 import 'dart:convert';
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-
-// define a model class this time
-class Person {
-  final String name;
-  final int age;
-  final String email;
-  final Map<String,dynamic> address;
-
-  const Person({
-    required this.name,
-    required this.age,
-    required this.email,
-    required this.address,
-  });
-
-  // know how to load ourself from JSON
-  factory Person.fromJson(Map<String,dynamic> json) {
-    return Person(
-      name: json['name'] as String,
-      age: json['age'] as int,
-      email: json['email'] as String,
-      address: json['address'] as Map<String,dynamic>,
-    );
-  }
-}
-
+import 'package:path_provider/path_provider.dart';
+import '../models/person.dart';
 
 class App4 extends StatelessWidget {
   const App4({super.key});
 
-  // load the data from the asset file (asynchronously)
-  Future<Person> _loadData(BuildContext context) async {
-    final data = await DefaultAssetBundle.of(context)
-      .loadString('assets/data.json');
-
-    // let's pretend this takes a while ...  
-    await Future.delayed(const Duration(seconds: 2));
-
-    return Person.fromJson(json.decode(data));
-  }
-
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      // wrap the widget tree in a FutureProvider, which will provide
-      // the data to any child widgets that request it (after the future
-      // completes)
-      home: FutureProvider<Person?>(
-        create: (context) => _loadData(context),
-        initialData: null,
-        child: const PersonView()
-      ),
+    return const MaterialApp(
+      home: PersonView(),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
 
-
-class PersonView extends StatelessWidget {
+class PersonView extends StatefulWidget {
   const PersonView({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    // get the data from the provider --- it may be null if the future
-    // hasn't completed yet
-    final person = Provider.of<Person?>(context);
+  State<PersonView> createState() => _PersonViewState();
+}
 
-    if (person == null) {
-      return const Scaffold(
-        body: Center(
-          child: CircularProgressIndicator(),
-        ),
-      );
-    } else {
-      return Scaffold(
-        body: ListView(
-          children: [
-            ListTile(
-              title: const Text('Name'),
-              subtitle: Text(person.name),
-            ),
-            ListTile(
-              title: const Text('Age'),
-              subtitle: Text(person.age.toString()),
-            ),
-            ListTile(
-              title: const Text('Email'),
-              subtitle: Text(person.email),
-            ),
-            ListTile(
-              title: const Text('Address'),
-              subtitle: Text(person.address.toString()),
-            ),
-          ],
-        ),
-      );
+class _PersonViewState extends State<PersonView> {
+  late Future<Person> _person;
+
+  @override
+  initState() {
+    super.initState();
+    _person = _loadData(context);
+  }
+
+  Future<String> _dataFilePath() async {
+    final dir = await getApplicationDocumentsDirectory();
+    return '${dir.path}/data.json';
+  }
+
+  Future<Person> _loadData(BuildContext context, {bool reset = false}) async {
+    var data =
+        await DefaultAssetBundle.of(context).loadString('assets/data.json');
+
+    final file = File(await _dataFilePath());
+
+    if (reset) {
+      // overwrite the file with the asset data
+      await file.writeAsString(data);
+    } else if (file.existsSync()) {
+      // otherwise, use updated file (if it exists)
+      data = await file.readAsString();
     }
+
+    return Person.fromJson(json.decode(data));
+  }
+
+  Future<void> _writeData(Person person) async {
+    final file = File(await _dataFilePath());
+    await file.writeAsString(json.encode(person.toJson()));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder<Person>(
+        future: _person,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Scaffold(
+              body: Center(
+                child: CircularProgressIndicator(),
+              ),
+            );
+          } else {
+            final person = snapshot.data as Person;
+            return Scaffold(
+              appBar: AppBar(
+                title: const Text('Person Info'),
+                actions: [
+                  IconButton(
+                    icon: const Icon(Icons.download),
+                    onPressed: () async {
+                      setState(() {
+                        _person = _loadData(context, reset: true);
+                      });
+                    },
+                  ),
+                ],
+              ),
+              body: ListView(
+                children: [
+                  EditableListTile(
+                      title: 'Name',
+                      value: person.name,
+                      onChanged: (name) async {
+                        person.name = name;
+                        await _writeData(person);
+                        setState(() {
+                          _person = Future.value(person);
+                        });
+                      }),
+                  EditableListTile(
+                      title: 'Age',
+                      value: person.age.toString(),
+                      onChanged: (age) async {
+                        person.age = int.parse(age);
+                        await _writeData(person);
+                        setState(() {
+                          _person = Future.value(person);
+                        });
+                      }),
+                  EditableListTile(
+                      title: 'Email',
+                      value: person.email,
+                      onChanged: (email) async {
+                        person.email = email;
+                        await _writeData(person);
+                        setState(() {
+                          _person = Future.value(person);
+                        });
+                      }),
+                  ListTile(
+                    title: const Text('Address'),
+                    subtitle: Text(person.address.toString()),
+                  ),
+                ],
+              ),
+            );
+          }
+        });
+  }
+}
+
+class EditableListTile extends StatelessWidget {
+  final String title;
+  final String value;
+  final void Function(String) onChanged;
+
+  const EditableListTile({
+    required this.title,
+    required this.value,
+    required this.onChanged,
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      title: Text(title),
+      subtitle: Text(value),
+      onTap: () async {
+        final newValue = await showDialog<String>(
+          context: context,
+          builder: (context) {
+            final controller = TextEditingController(text: value);
+            return AlertDialog(
+              title: Text('Edit $title'),
+              content: TextField(controller: controller),
+              actions: [
+                TextButton(
+                  onPressed: () => Navigator.pop(context),
+                  child: const Text('Cancel'),
+                ),
+                TextButton(
+                  onPressed: () => Navigator.pop(context, controller.text),
+                  child: const Text('Save'),
+                ),
+              ],
+            );
+          },
+        );
+
+        if (newValue != null) {
+          onChanged(newValue);
+        }
+      },
+    );
   }
 }
