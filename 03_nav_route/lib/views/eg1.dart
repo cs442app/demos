10e128bf28c7c7ef7b337d055a20c6b82e191cb6
Michael Lee <lee@soi2.org>
2025-02-21 09:05:37 -0600

    Removing nav implementation

--- 03_nav_route/lib/views/eg1.dart
+++ 03_nav_route/lib/views/eg1.dart
@@ -11,7 +11,7 @@ class App1 extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       title: 'App 1',
-      // the home widget is the first page, or "route", to display -- 
+      // the home widget is the first page, or "route", to display --
       // it is at the bottom of the navigation stack
       home: Page1(),
     );
@@ -24,28 +24,15 @@ class Page1 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Page 1')),
-      body: Center(
-        child: TextButton(
-          child: const Text('Go to Page 2'),
-          onPressed: () {
-            // Push a new screen onto the navigation stack
-            // equivalent to Navigator.of(context).push(...)
-            Navigator.push(
-              context,
-              // MaterialPageRoute defines a modal (full screen) route
-              MaterialPageRoute(
-                // The builder function returns the widget tree
-                // to display in the new route
-                builder: (context) {
-                  return const Page2();
-                }
-              ),
-            );
-          },
-        ),
-      )
-    );
+        appBar: AppBar(title: const Text('Page 1')),
+        body: Center(
+          child: TextButton(
+            child: const Text('Go to Page 2'),
+            onPressed: () {
+              // Navigate to Page2
+            },
+          ),
+        ));
   }
 }
 
@@ -55,19 +42,16 @@ class Page2 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Page 2')),
-      body: Center(
-        // This button isn't really needed, since the AppBar
-        // automatically provides a "back" button
-        child: TextButton(
-          child: const Text('Go back to Page 1'),
-          onPressed: () {
-            // Pop the current screen off the navigation stack
-            // equivalent to Navigator.of(context).pop()
-            Navigator.pop(context);
-          },
-        ),
-      )
-    );
+        appBar: AppBar(title: const Text('Page 2')),
+        body: Center(
+          // This button isn't really needed, since the AppBar
+          // automatically provides a "back" button
+          child: TextButton(
+            child: const Text('Go back to Page 1'),
+            onPressed: () {
+              // Navigate back to Page1
+            },
+          ),
+        ));
   }
 }
