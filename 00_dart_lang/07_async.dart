f46db54f67766deb141cde903b2753e82e52f30f
Michael Lee <lee@soi2.org>
2025-02-26 09:33:36 -0600

    Fixing spacing/indentation

--- 00_dart_lang/07_async.dart
+++ 00_dart_lang/07_async.dart
@@ -2,7 +2,7 @@ import 'dart:async';
 
 void main() {
   print('main() started');
-  switch(1) {
+  switch (1) {
     case 1:
       asyncFunction1a();
       break;
@@ -29,24 +29,29 @@ void main() {
   print('main() finishing');
 }
 
-
 // obtain a future and runs a handler on its completion
 void asyncFunction1a() {
   print('asyncFunction1a() started');
+
   Future<int> future = Future<int>.delayed(Duration(seconds: 2), () {
     return 42;
   });
+
   future.then((result) => print('Future resolved with result: $result'));
+
   print('asyncFunction1a() finishing');
 }
 
 // same as above but using async/await
 void asyncFunction1b() async {
   print('asyncFunction1b() started');
+
   var result = await Future<int>.delayed(Duration(seconds: 2), () {
     return 42;
   });
+
   print('Future resolved with result: $result');
+
   print('asyncFunction1b() finishing');
 }
 
@@ -63,53 +68,72 @@ Future<int> asyncFunction2b() async {
 // obtain a future and deal with errors in its completion
 void asyncFunction3a() {
   print('asyncFunction3a() started');
+
   Future<int> future = Future<int>.delayed(Duration(seconds: 2), () {
     throw Exception('Error in asyncFunction3a()');
   });
-  future.then((result) => print('Future resolved with result: $result'))
-        .catchError((error) => print('Future failed with error: $error'));
+
+  future
+      .then((result) => print('Future resolved with result: $result'))
+      .catchError((error) => print('Future failed with error: $error'));
+
   print('asyncFunction3a() finishing');
 }
 
 // same as above but using async/await
 void asyncFunction3b() async {
   print('asyncFunction3b() started');
+
   try {
     var result = await Future<int>.delayed(Duration(seconds: 2), () {
       throw Exception('Error in asyncFunction3a()');
     });
+
     print('Future resolved with value: $result');
   } catch (error) {
     print('Future failed with error: $error');
   }
+
   print('asyncFunction3b() finishing');
 }
 
 // same as below but with explicit futures
 void asyncFunction4a() {
-Future<int> future1 = Future<int>.delayed(Duration(seconds: 1), () => 42);
-future1.then((result) {
-  print('First future resolved with result: $result');
-  Future<int> future2 = Future<int>.delayed(Duration(seconds: 1), () => 43);
-  future2.then((result) {
-    print('Second future resolved with result: $result');
-    Future<int> future3 = Future<int>.delayed(Duration(seconds: 1), () => 44);
-    future3.then((result) {
-      print('Third future resolved with result: $result');
-      print('asyncFunction4a() finishing');
+  print('asyncFunction4a() started');
+
+  Future<int> future1 = Future<int>.delayed(Duration(seconds: 1), () => 42);
+
+  future1.then((result) {
+    print('First future resolved with result: $result');
+
+    Future<int> future2 = Future<int>.delayed(Duration(seconds: 1), () => 43);
+
+    future2.then((result) {
+      print('Second future resolved with result: $result');
+
+      Future<int> future3 = Future<int>.delayed(Duration(seconds: 1), () => 44);
+
+      future3.then((result) {
+        print('Third future resolved with result: $result');
+      });
     });
   });
-});
+
+  print('asyncFunction4a() finishing');
 }
 
 // multiple futures
 void asyncFunction4b() async {
-  print('asyncFunction4() started');
-var result = await Future.delayed(Duration(seconds: 1), () => 42);
-print('First future resolved with result: $result');
-result = await Future.delayed(Duration(seconds: 1), () => 43);
-print('Second future resolved with result: $result');
-result = await Future.delayed(Duration(seconds: 1), () => 44);
-print('Third future resolved with result: $result');
-print('asyncFunction4() finishing');
+  print('asyncFunction4b() started');
+
+  var result = await Future.delayed(Duration(seconds: 1), () => 42);
+  print('First future resolved with result: $result');
+
+  result = await Future.delayed(Duration(seconds: 1), () => 43);
+  print('Second future resolved with result: $result');
+
+  result = await Future.delayed(Duration(seconds: 1), () => 44);
+  print('Third future resolved with result: $result');
+
+  print('asyncFunction4b() finishing');
 }
